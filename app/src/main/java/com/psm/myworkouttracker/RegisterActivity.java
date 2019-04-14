e97dc6f1c73b79d26e7d40de7a5670196ad1932f
amiruddin6521 <amiruddin6521@gmail.com>
2019-04-14 21:56:21 +0800

    V3

--- app/src/main/java/com/psm/myworkouttracker/RegisterActivity.java
+++ app/src/main/java/com/psm/myworkouttracker/RegisterActivity.java
@@ -1,13 +1,207 @@
 package com.psm.myworkouttracker;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private CircularImageView roundProfile;
+    private FloatingActionButton photoButton;
+    private EditText txtName, txtEmail, txtPassword, txtWeight, txtHeight;
+    private TextView txtBirthday;
+    private RadioGroup radGender;
+    private RadioButton radMale, radFemale;
+    private Button btnSave;
+    private ImageButton btnCalender;
+    private String userChoosenTask;
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        roundProfile =  findViewById(R.id.photo);
+        photoButton = findViewById(R.id.actionCamera);
+        txtName = findViewById(R.id.txtName);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtWeight = findViewById(R.id.txtWeight);
+        txtHeight =  findViewById(R.id.txtHeight);
+        txtBirthday = findViewById(R.id.txtBirthday);
+        btnSave = findViewById(R.id.btnSave);
+        btnCalender = findViewById(R.id.btnCalender);
+        radGender = findViewById(R.id.radGender);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getRadioGender();
+            }
+        });
+
+        btnCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calenderPicker();
+            }
+        });
+    }
+
+    //Get brithday date
+    private void calenderPicker() {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        txtBirthday.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+    //Get gender data
+    private String getRadioGender() {
+        // get selected radio button from radioGroup
+        int selectedId = radGender.getCheckedRadioButtonId();
+
+        // find the radiobutton by returned id
+        radMale = findViewById(selectedId);
+
+        String result = radMale.getText().toString();
+
+        Toast.makeText(RegisterActivity.this, result, Toast.LENGTH_SHORT).show();
+        return result;
+    }
+
+    //Dialog box with the three options
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask="Take Photo";
+                    cameraIntent();
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask="Choose from Library";
+                    galleryIntent();
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    //Calling Implicit Intent to open the camera application on user's phone
+    private void cameraIntent()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    //Calling an implicit intent to open the gallery
+    private void galleryIntent()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"),2);
+    }
+
+    //Handle the result we have received by calling startActivityForResult()
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 2)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == 1)
+                onCaptureImageResult(data);
+        }
+    }
+
+    //Handle for select from gallery
+    private void onSelectFromGalleryResult(Intent data) {
+        Bitmap bm=null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        roundProfile.setImageBitmap(bm);
+    }
+
+    //Handle for image capture from camera
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        roundProfile.setImageBitmap(thumbnail);
     }
 }
